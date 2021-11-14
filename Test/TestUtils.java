7a9d54369546747156b5e7f1f2d177c532df4736
Marrylin <marrylin2019@outlook.com>
2021-11-14 11:56:14 +0800

    finish an method and add two utils

--- Test/TestUtils.java
+++ Test/TestUtils.java
@@ -4,4 +4,12 @@ public class TestUtils {
     public static Integer getNormalValue(int min, int max) {
         return new Random(System.currentTimeMillis()).nextInt(max)%(max-min+1) + min;
     }
+
+    public static int max(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+
+    public static int min(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
 }
--- Test/VolumeCalculatorTestCasesGenerator.java
+++ Test/VolumeCalculatorTestCasesGenerator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class VolumeCalculatorTestCasesGenerator {
     /**   getVolume(length={0}, width={1}, height={2})={3}  **/
@@ -7,6 +8,7 @@ public class VolumeCalculatorTestCasesGenerator {
     final static int[] boundaryVals = {2, 100, 5, 200, 20, 80};
 
     public static ArrayList<Integer[] > generateBoundaryTestCases() {
+        // 总测试样例数为4*NUM_OF_VAR+1=13
         final int NUM_OF_BSV = 4;  // Boundary Test Special Value
 
         ArrayList<Integer[] > result = new ArrayList<>();
@@ -34,6 +36,7 @@ public class VolumeCalculatorTestCasesGenerator {
     }
 
     public static ArrayList<Integer[] > generateRobustnessTestCases() {
+        // 总测试样例数为6*NUM_OF_VAR+1=19
         final int NUM_OF_RSV = 6;  // Boundary Test Special Value
 
         ArrayList<Integer[] > result = new ArrayList<>();
@@ -61,6 +64,7 @@ public class VolumeCalculatorTestCasesGenerator {
     }
 
     public static ArrayList<Integer[] > generateWorstCaseTestCases() {
+        // 总测试样例数为5^NUM_OF_VAR=125
         final int NUM = 5;  // Worst Case Test All values number
         Integer[][] tmpVals = new Integer[NUM_OF_VAR][NUM];
 
@@ -68,7 +72,7 @@ public class VolumeCalculatorTestCasesGenerator {
         for (int iVar=0; iVar < NUM_OF_VAR; iVar++)
             //                            min,                  min+,                   max-,                     max,                    normal
             tmpVals[iVar] = new Integer[]{boundaryVals[2*iVar], boundaryVals[2*iVar]+1, boundaryVals[2*iVar+1]-1, boundaryVals[2*iVar+1], TestUtils.getNormalValue(boundaryVals[2*iVar], boundaryVals[2*iVar+1])};
-        // worst case test需要5^n个测试样例，即
+        // worst case test需要5^n个测试样例，即对于每个变量都遍历全部的取值（对于worst case test来说是5个），因此做3重循环
         for (int i=0; i < NUM; i++)
             for (int j=0; j < NUM; j++)
                 for (int k=0; k < NUM; k++) {
@@ -79,6 +83,7 @@ public class VolumeCalculatorTestCasesGenerator {
     }
 
     public static ArrayList<Integer[] > generateRobustWorstCaseTestCases() {
+        // 总测试样例数为7^NUM_OF_VAR+1=343
         final int NUM = 7;  // Worst Case Test All values number
         Integer[][] tmpVals = new Integer[NUM_OF_VAR][NUM];
 
@@ -97,12 +102,88 @@ public class VolumeCalculatorTestCasesGenerator {
     }
 
     public static ArrayList<Integer[] > generateStrongNormalEquivalenceClassTestCases() {
+        // 总测试样例数为3
+        // 此等价类划分依据结果进行
+        // 划分为正方体体积，柱体体积及普通长方体(三边均不相等)体积
+        // 由此，结果导向的等价类划分只有一个变量，因此测试样例数为3
         ArrayList<Integer[]> result = new ArrayList<>();
+        /** cube **/
+        // 生成正方体的边长，算法为取三边长度范围的左边界值中最大值为随机数函数的下界，右边界值中的最小值为上界
+        int sideLen = TestUtils.getNormalValue(TestUtils.max(boundaryVals[0], boundaryVals[2], boundaryVals[4]), TestUtils.min(boundaryVals[1], boundaryVals[3], boundaryVals[5]));
+        result.add(new Integer[]{sideLen, sideLen, sideLen, StdVolumeCalculator.getVolume(sideLen, sideLen, sideLen)});
+        /** cylinder **/
+        Integer[] tmpDate = new Integer[NUM_OF_VAR+1];
+        // 随机选出一条不相等的边，生成相等边的长度，算法同上
+        int notEqualSideId = TestUtils.getNormalValue(0, 2),
+            equalSideLen = TestUtils.getNormalValue(TestUtils.max(boundaryVals[0], boundaryVals[2], boundaryVals[4]), TestUtils.min(boundaryVals[1], boundaryVals[3], boundaryVals[5]));
+        // 先给三条边都赋相同的值
+        for (int iVar=0; iVar < NUM_OF_VAR; iVar++)
+            tmpDate[iVar] = equalSideLen;
+        // 给选出的不相等边赋不相等的值
+        do
+            tmpDate[notEqualSideId] =  TestUtils.getNormalValue(boundaryVals[notEqualSideId]*2, boundaryVals[notEqualSideId]*2+1);
+        while (tmpDate[notEqualSideId].equals(tmpDate[(notEqualSideId+1)%NUM_OF_VAR])); // 使用不溢出加法判定特值边长与特值边的下一个边长是否相等
+        tmpDate[NUM_OF_VAR] = StdVolumeCalculator.getVolume(tmpDate[0], tmpDate[1], tmpDate[2]);
+        result.add(tmpDate);
+        /** Ordinary rectangle **/
+        Integer[] randomSet = new Integer[NUM_OF_VAR+1];
+        for (int iVar = 0; iVar < NUM_OF_VAR; iVar++) {
+            boolean flag;
+            Integer tmp;
+            do {
+                flag = false;
+                tmp = TestUtils.getNormalValue(boundaryVals[iVar * 2], boundaryVals[iVar * 2 + 1]);
+                for (int i=0; i < iVar; i++) if (tmp.equals(randomSet[i])) { flag=true; break;}
+            } while (flag);
+            randomSet[iVar] = tmp;
+        }
+        randomSet[NUM_OF_VAR] = StdVolumeCalculator.getVolume(randomSet[0], randomSet[1], randomSet[2]);
+        result.add(randomSet);
+
         return result;
     }
 
     public static ArrayList<Integer[] > generateStrongRobustEquivalenceClassTestCases() {
+        // 总测试样例数为4
+        // 此等价类划分依据结果进行
+        // 划分为正方体体积，柱体体积，普通长方体（三边均不相等）体积及错误数据（不为长方体）体积
+        // 且结果导向的等价类划分只有一个变量，因此测试样例数为4
         ArrayList<Integer[]> result = new ArrayList<>();
+        /** cube **/
+        // 生成正方体的边长，算法为取三边长度范围的左边界值中最大值为随机数函数的下界，右边界值中的最小值为上界
+        int sideLen = TestUtils.getNormalValue(TestUtils.max(boundaryVals[0], boundaryVals[2], boundaryVals[4]), TestUtils.min(boundaryVals[1], boundaryVals[3], boundaryVals[5]));
+        result.add(new Integer[]{sideLen, sideLen, sideLen, StdVolumeCalculator.getVolume(sideLen, sideLen, sideLen)});
+        /** cylinder **/
+        Integer[] tmpDate = new Integer[NUM_OF_VAR+1];
+        // 随机选出一条不相等的边，生成相等边的长度，算法同上
+        int notEqualSideId = TestUtils.getNormalValue(0, 2),
+                equalSideLen = TestUtils.getNormalValue(TestUtils.max(boundaryVals[0], boundaryVals[2], boundaryVals[4]), TestUtils.min(boundaryVals[1], boundaryVals[3], boundaryVals[5]));
+        // 先给三条边都赋相同的值
+        for (int iVar=0; iVar < NUM_OF_VAR; iVar++)
+            tmpDate[iVar] = equalSideLen;
+        // 给选出的不相等边赋不相等的值
+        do
+            tmpDate[notEqualSideId] =  TestUtils.getNormalValue(boundaryVals[notEqualSideId]*2, boundaryVals[notEqualSideId]*2+1);
+        while (tmpDate[notEqualSideId].equals(tmpDate[(notEqualSideId+1)%NUM_OF_VAR])); // 使用不溢出加法判定特值边长与特值边的下一个边长是否相等
+        tmpDate[NUM_OF_VAR] = StdVolumeCalculator.getVolume(tmpDate[0], tmpDate[1], tmpDate[2]);
+        result.add(tmpDate);
+        /** Ordinary rectangle **/
+        Integer[] randomSet = new Integer[NUM_OF_VAR+1];
+        for (int iVar = 0; iVar < NUM_OF_VAR; iVar++) {
+            boolean flag;
+            Integer tmp;
+            do {
+                flag = false;
+                tmp = TestUtils.getNormalValue(boundaryVals[iVar * 2], boundaryVals[iVar * 2 + 1]);
+                for (int i=0; i < iVar; i++) if (tmp.equals(randomSet[i])) { flag=true; break;}
+            } while (flag);
+            randomSet[iVar] = tmp;
+        }
+        randomSet[NUM_OF_VAR] = StdVolumeCalculator.getVolume(randomSet[0], randomSet[1], randomSet[2]);
+        result.add(randomSet);
+        /** Not for cuboid **/
+        result.add(new Integer[]{0, -1, 1000, StdVolumeCalculator.getVolume(0, -1, 1000)});
+
         return result;
     }
 
